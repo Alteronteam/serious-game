50794b8dbfd0a2bbbe12fc4df762e3b42bfad234
bebeto107 <albrtpc@gmail.com>
2026-06-25 19:05:46 -0300

    correção rapida

--- game_over.gd
+++ game_over.gd
@@ -9,8 +9,10 @@ func _ready() -> void:
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://menu.tscn")
 	Global.hp = 4.0
+	Global.levels = 0
 
 
 func _on_play_again_pressed() -> void:
 	get_tree().change_scene_to_file("res://world.tscn")
 	Global.hp = 4.0
+	Global.levels = 0
