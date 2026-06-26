eaa368dc1a51763c5412484b6d591716a1b63cdb
bebeto107 <albrtpc@gmail.com>
2026-06-26 17:05:48 -0300

    correção de bugs fases

--- game_over.gd
+++ game_over.gd
@@ -23,3 +23,5 @@ func  Next():
 			get_tree().change_scene_to_file("res://world_2.tscn")
 		2:
 			get_tree().change_scene_to_file("res://world_3.tscn")
+		3:
+			get_tree().change_scene_to_file("res://world_4.tscn")
